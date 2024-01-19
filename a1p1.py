b1a74ad17fe409ae4fbd3f4d6fbdaa47e9e0b098
Lex Ibanez <laibanez@uci.edu>
2024-01-19 15:18:00 -0800

    added -s option and -s with -r option

--- a1p1.py
+++ a1p1.py
@@ -1,17 +1,35 @@
 from pathlib import Path
+# sample paths
+# C:\Users\lexib\OneDrive\Desktop\ICS32
+# C:\Users\lexib\OneDrive\Desktop\test
 
 def display_directory(directory, options):
+    
+
     contents = list(directory.iterdir())
 
     files = [x for x in contents if x.is_file()] # sort contents for files
     directories = [x for x in contents if x.is_dir()] # sort contents for directories
 
-    if '-f' in options:
+    if '-s' in options:
+        if len(options) == 3:
+            search_file = options[2] # gets the file to be searched
+        if len(options) == 4:
+            search_file = options[3] # gets the file to be searched
+
+        if Path(directory / search_file).is_file(): # checks if the search file has a path
+            print(Path(directory / search_file))
+        for directory in directories:
+            if '-r' in options:
+                display_directory(directory, options)
+
+    elif '-f' in options:
         for file in files:
             print(file)
         for directory in directories:
             if '-r' in options:
                 display_directory(directory, options)
+
     else:
         for file in files:
             print(file)
@@ -32,15 +50,14 @@ def main():
             if len(args) > 1:
                 options = args
             directory = Path(args[0])
+            if directory.is_dir():
+                display_directory(directory, options)
+            else:
+                print('Could not find directory.')
         else:
             print("Invalid command.")
             continue
 
-        if directory.is_dir():
-            display_directory(directory, options)
-        else:
-            print('Could not find directory.')
-                
-            
+        
 if __name__ == '__main__':
     main()
