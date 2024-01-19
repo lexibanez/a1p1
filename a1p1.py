6a2f41ebca48932623f717d36067395c48b4f312
Lex Ibanez <laibanez@uci.edu>
2024-01-19 11:07:30 -0800

    added L command functionality

--- a1p1.py
+++ a1p1.py
@@ -1,1 +1,26 @@
-print("hello world")
+from pathlib import Path
+
+def main():
+    
+    while True:
+        user_input = input("Enter Command: ")
+        command, *args = user_input.split()
+        option = None
+
+        if command == 'Q':
+            break
+        elif command == 'L' and option is None:
+            directory = Path(args[0])
+            if directory.is_dir():
+                contents = list(directory.iterdir())
+                files = [x for x in contents if x.is_file()]
+                directories = [x for x in contents if x.is_dir()]
+
+                for file in files:
+                    print(file)
+                for directory in directories:
+                    print(directory)
+
+
+if __name__ == '__main__':
+    main()
