5056c83e5babc5c410b4933b6fdd48af2f2c7b7b
Lex Ibanez <laibanez@uci.edu>
2024-01-19 14:37:03 -0800

    added -f option and -f with -r combined

--- a1p1.py
+++ a1p1.py
@@ -1,37 +1,43 @@
 from pathlib import Path
 
-def list_directory(directory, option=None):
-    contents = list(directory.iterdir())\
-    
+def display_directory(directory, options):
+    contents = list(directory.iterdir())
+
     files = [x for x in contents if x.is_file()] # sort contents for files
-    for file in files:
-        print(file)
-
     directories = [x for x in contents if x.is_dir()] # sort contents for directories
-    for directory in directories:
-        print(directory)
-        if option == '-r':
-            list_directory(directory, '-r')
 
-        
+    if '-f' in options:
+        for file in files:
+            print(file)
+        for directory in directories:
+            if '-r' in options:
+                display_directory(directory, options)
+    else:
+        for file in files:
+            print(file)
+        for directory in directories:
+            print(directory)
+            if '-r' in options:
+                display_directory(directory, options)
+
 def main():
     while True:
         user_input = input("Enter Command: ")
         command, *args = user_input.split()
-
+        
         if command == 'Q':
             break
         elif command == 'L': 
-            option = None
+            options = []
             if len(args) > 1:
-                option = args[1]
+                options = args
             directory = Path(args[0])
         else:
             print("Invalid command.")
             continue
 
         if directory.is_dir():
-            list_directory(directory, option)
+            display_directory(directory, options)
         else:
             print('Could not find directory.')
                 
