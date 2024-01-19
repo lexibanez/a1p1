c92df5c75104342e9d0d3130fa74839b28a404c0
Lex Ibanez <laibanez@uci.edu>
2024-01-19 13:25:33 -0800

    created list_directory function and functionality for -r option

--- a1p1.py
+++ a1p1.py
@@ -1,26 +1,40 @@
 from pathlib import Path
 
-def main():
+def list_directory(directory, option=None):
+    contents = list(directory.iterdir())\
     
+    files = [x for x in contents if x.is_file()] # sort contents for files
+    for file in files:
+        print(file)
+
+    directories = [x for x in contents if x.is_dir()] # sort contents for directories
+    for directory in directories:
+        print(directory)
+        if option == '-r':
+            list_directory(directory, '-r')
+
+        
+def main():
     while True:
         user_input = input("Enter Command: ")
         command, *args = user_input.split()
-        option = None
 
         if command == 'Q':
             break
-        elif command == 'L' and option is None:
+        elif command == 'L': 
+            option = None
+            if len(args) > 1:
+                option = args[1]
             directory = Path(args[0])
-            if directory.is_dir():
-                contents = list(directory.iterdir())
-                files = [x for x in contents if x.is_file()]
-                directories = [x for x in contents if x.is_dir()]
+        else:
+            print("Invalid command.")
+            continue
 
-                for file in files:
-                    print(file)
-                for directory in directories:
-                    print(directory)
-
-
+        if directory.is_dir():
+            list_directory(directory, option)
+        else:
+            print('Could not find directory.')
+                
+            
 if __name__ == '__main__':
     main()
